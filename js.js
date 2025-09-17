e97d7d9946ece25bd36a8db29939560e9dbf69e1
diana <@dianayahyaei2005@gmail.com>
2025-09-17 13:57:32 +0330

    while

--- js.js
+++ js.js
@@ -1,15 +1,27 @@
 function isPrimeNumber(num) {
-  if (num < 2) {
-    return false;
-  }
-  if (num <= 3) {
-    return true;
-  }
-  if (num % 2 === 0 || num % 3 === 0) {
-    return false;
-  } else {
-    return true;
+  while (
+    (num % 2 === 0,
+    num % 3 === 0,
+    num % 4 !== 0,
+    num % 5 === 0,
+    num % 6 !== 0,
+    num % 7 === 0,
+    num % 8 !== 0,
+    num % 9 !== 0,
+    num % 10 !== 0,
+    num % 11 === 0,
+    num % 12 !== 0,
+    num % 13 === 0,
+    num % 14 !== 0,
+    num % 15 !== 0,
+    num % 16 !== 0,
+    num % 17 === 0,
+    num % 18 !== 0,
+    num % 19 === 0,
+    num % 20 !== 0)
+  ) {
+    console.log("it is prim");
   }
 }
 
-console.log(isPrimeNumber(5));
+console.log(isPrimeNumber(8));
